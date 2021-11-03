00d879543763672303a8c591e70ed22f5fb40e7a
usr_admin <usr_admin@hpe-cp-cli01.globalsysco.local>
2021-11-03 17:50:20 -0600

    Prueba

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-echo phpinfo();
-echo "Hola Mundo !!!"
+//!--echo phpinfo();
+//echo "Hola Mundo !!!"
+$servername = "mysql-service"; // Nombre/IP del servidor
+$database = "test"; // Nombre de la BBDD
+$username = "root"; // Nombre del usuario
+$password = "mysq_l12345"; // Contraseña del usuario
+// Creamos la conexión
+$con = mysqli_connect($servername, $username, $password, $database);
+// Comprobamos la conexión
+if (!$con) {
+    die("La conexión ha fallado: " . mysqli_connect_error());
+}
+echo "Conexión satisfactoria";
+mysqli_close($con);
 ?>
