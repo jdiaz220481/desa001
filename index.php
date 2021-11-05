cc16628ba0055e48a271358507a64c0a9dac5d30
usr_admin <usr_admin@hpe-cp-cli01.globalsysco.local>
2021-11-05 15:09:08 -0600

    Prueba

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //!--echo phpinfo();
 //echo "Hola Mundo !!!"
-$servername = "mysql-service"; // Nombre/IP del servidor
+/*$servername = "mysql-service"; // Nombre/IP del servidor
 $database = "test"; // Nombre de la BBDD
 $username = "root"; // Nombre del usuario
 $password = "mysq_l12345"; // Contraseña del usuario
@@ -12,5 +12,23 @@ if (!$con) {
     die("La conexión ha fallado: " . mysqli_connect_error());
 }
 echo "Conexión satisfactoria";
-mysqli_close($con);
+mysqli_close($con);*/
+
+phpinfo();
+echo "MySQL";
+$host = "mysql-service";
+$dbname = "test";
+$username = "root";
+$password = "mysq_l12345";
+$conn;
+// get the database connection
+$conn = null;
+ try {
+      $conn = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $username, $password);
+      $conn->exec("SET CHARACTER SET utf8");
+      print(" conexion existosa ");
+    } catch (PDOException $exception) {
+ echo "Connection error check pro messages: " . $exception->getMessage();
+}
+
 ?>
